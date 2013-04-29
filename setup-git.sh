d4e9ef3cfe9476139eb917bb26bb8aad68c3e5fa
Wang Luming <wlm199558@126.com>
2013-04-29 15:44:48 +0800

    set-up git script

---
+++ setup-git.sh
@@ -0,0 +1,8 @@
+# Sets the default name for git to use when you commit
+git config --global user.name "Wang Luming"
+
+# Sets the default email for git to use when you commit
+git config --global user.email "wlm199558@126.com"
+
+# Set git to use the credential memory cache
+git config --global credential.helper cache
